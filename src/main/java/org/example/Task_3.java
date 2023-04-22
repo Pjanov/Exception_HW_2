176a8ab6a92ee3775a047156681819646b170912
pjanov <pjanov84@mail.ru>
2023-04-22 14:38:11 +0300

    Task_3

---
+++ src/main/java/org/example/Task_3.java
@@ -0,0 +1,29 @@
+package org.example;
+
+/**
+ * Дан следующий код, исправьте его там, где требуется:
+ * задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit
+ */
+public class Task_3 {
+    public static void main(String[] args) {
+
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = {1, 2};
+            abc[2] = 9;
+        } catch (ArithmeticException ex) {
+            System.out.println("Нельзя делить на ноль!");
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Индекс выходит за пределы массива!");
+        } catch (Exception ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void printSum(Integer a, Integer b) {
+        System.out.println(a + b);
+    }
+}
